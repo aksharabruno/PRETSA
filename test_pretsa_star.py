4765a5c327f2913b873f0c71eec934476baf20f5
aksharabruno <aksharabruno0119@gmail.com>
2025-03-18 16:21:07 +0100

    Update test

--- test_pretsa_star.py
+++ test_pretsa_star.py
@@ -17,7 +17,6 @@ class TestPretsaStar(unittest.TestCase):
         self.pretsa_star = Pretsa_star(self.eventLog)
     
     def testCheckHomogeneousNodes(self):
-        # Create a tree with homogeneous nodes
         root = Node("root", cases=set(['1', '2', '3']))
         child1 = Node("child1", parent=root, cases=set(['1']))
         child2 = Node("child2", parent=root, cases=set(['2']))
@@ -28,27 +27,24 @@ class TestPretsaStar(unittest.TestCase):
             '2': 'High',
             '3': 'High'
         }
+
+        self.pretsa_star._modify_data_to_increase_diversity = MagicMock()
         self.pretsa_star._checkHomogenousNodes(root)
-        #with self.assertRaises(Exception) as context:   #need to change this test
-        #    self.pretsa_star._checkHomogenousNodes(root)
-        #self.assertTrue('Homogeneity attack detected! The tree contains homogeneous nodes.' in str(context.exception))
-       
+        self.pretsa_star._modify_data_to_increase_diversity.assert_called()
+        self.assertEqual(self.pretsa_star._modify_data_to_increase_diversity.call_count, 3)
+
     def testReplayAttack(self):
-        # Mock parts of runPretsa that are not relevant to nonce validation
         self.pretsa_star._addDifferentialPrivateNosieToEnsureTCloseness = MagicMock()
         self.pretsa_star._checkPrivacyLevel = MagicMock()
         self.pretsa_star._checkLDiversity = MagicMock(return_value=False)
 
-        # Generate a nonce and run Pretsa
         nonce1 = self.pretsa_star._generate_nonce()
         self.pretsa_star._validate_nonce(nonce1)
 
-        # Try to reuse the same nonce
         with self.assertRaises(Exception) as context:
             self.pretsa_star._validate_nonce(nonce1)
         self.assertTrue('Replay attack detected! Nonce has already been used.' in str(context.exception))
 
-        # Generate a new nonce and run Pretsa
         nonce2 = str(uuid.uuid4())
         self.pretsa_star._validate_nonce(nonce2)
     
